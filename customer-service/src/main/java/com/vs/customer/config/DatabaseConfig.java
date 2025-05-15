ed06e1bc04fa385b19e0041ad21172d244a4d331
Dante Chavez <drcv.work.code@gmail.com>
2025-05-15 17:09:51 -0600

    sarabia corregido

---
+++ customer-service/src/main/java/com/vs/customer/config/DatabaseConfig.java
@@ -0,0 +1,57 @@
+package com.vs.customer.config;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import oracle.soda.OracleDatabase;
+import oracle.soda.OracleException;
+import oracle.soda.rdbms.OracleRDBMSClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Bean
+    OracleRDBMSClient oracleRDBMSClient() {
+        return new OracleRDBMSClient();
+    }
+
+    @Bean
+    OracleDatabase oracleDatabase() throws OracleException, SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, userName, password);
+        Properties props = new Properties();
+        props.put("oracle.soda.sharedMetadataCache", "true");
+        OracleDatabase oracleDatabase = new OracleRDBMSClient(props).getDatabase(connection);
+        connection.close();
+        return oracleDatabase;
+    }
+}
+
