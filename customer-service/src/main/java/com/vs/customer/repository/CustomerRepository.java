656dae609c5ffa96ddc9fdf4379bc51f87bc3d43
Dante Chavez <drcv.work.code@gmail.com>
2025-05-16 21:56:15 -0600

    customer-servcice corregidov2

---
+++ customer-service/src/main/java/com/vs/customer/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.vs.customer.repository;
+
+import com.vs.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
